5863e759d822678313268add317b3f6bced5888e
GreatPyrenessLover <kchuck.1022@gmail.com>
2026-06-07 11:49:22 -0400

    Fix video file names to match casing
    Updated video file names to lowercase.

--- data.js
+++ data.js
@@ -10,13 +10,13 @@ const DATA = {
     {
       title: "Easter Awards 2026",
       description: "The Results",
-      file: "videos/EasterAwards2026.mp4",
+      file: "videos/easterawards2026.mp4",
       thumbnail: "thumbs/easterawards.png"
     },
     {
       title: "Christmas Awards 2025",
       description: "The Results",
-      file: "videos/ChristmasAwards2025.mp4",
+      file: "videos/christmasawards2025.mp4",
       thumbnail: "thumbs/christmasawards.png"
     }
   ]

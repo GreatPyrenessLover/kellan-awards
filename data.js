c8fd434ca1cd2c3b7e1049af222c51fe2e11fd3a
GreatPyrenessLover <kchuck.1022@gmail.com>
2026-06-07 11:34:01 -0400

    Add DATA object with awards information

---
+++ data.js
@@ -0,0 +1,23 @@
+const DATA = {
+  password: "awards",
+
+  channel: {
+    name: "Kellan Awards",
+    banner: "kellanawards.png"
+  },
+
+  videos: [
+    {
+      title: "Easter Awards 2026",
+      description: "The Results",
+      file: "videos/EasterAwards2026.mp4",
+      thumbnail: "thumbs/easterawards.png"
+    },
+    {
+      title: "Christmas Awards 2025",
+      description: "The Results",
+      file: "videos/ChristmasAwards2025.mp4",
+      thumbnail: "thumbs/christmasawards.png"
+    }
+  ]
+};
